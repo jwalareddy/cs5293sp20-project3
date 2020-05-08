8971dbf237fdafed1ea417f8bd6689d3252bb4de
Jwala Reddy <jwalareddy34@gmail.com>
2020-05-08 15:59:54 +0000

    finalcommit

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from sklearn.ensemble import RandomForestClassifierimport codecs
 import json
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics.pairwise import cosine_similarity
 test_data = []
 training_data = []
 meal = []
@@ -38,21 +39,30 @@ train_data['concat_ingredients'] = train_data['ingredients'].map(';'.join)
 test_data['concat_ingredients'] = test_data['ingredients'].map(';'.join)
 train_data.head()
 def create_separate_lists(filename):
-    with codecs.open( filename,encoding = 'utf-8') as req:
+    with open( filename,encoding = 'utf-8') as req:
         data = json.load(req)
-    for i in range(0,len(data)):
-        meal.append(data[i]["id"])
-        cuisine.append(data[i]["cuisine"])
-        ingredients.append(data[i]["ingredients"])    
+    data_frame = pd.DataFrame({'id':dish_id,'cuisine':cuisine,'ingredients':ingredients})
+    data_frame['cuisine'].value_counts()
+    for ingr in data_frame['ingredients']:
+        ingr = ' '.join(ingr)
+        ingredients.append(ingr)
     for i in ingredients:
-        value1 =u''
-        for value2 in range(len(i)):
-            value1 = value1+u" "+i[value2]
-        individual_ingredients.append(value1.encode('utf-8'))    
-    return meal,cuisine,ingredients,individual_ingredients
+        individual_ingredients.append(u''.encode('utf-8')
+    for meal1 in data_frame['id']:
+        meal1=''.join(meal1)
+        meal.append(meal1)
+    for v in data_frame['cuisine']:
+        v = ''.join(v)
+        cuisine.append(v)
+    return meal,cuisine, individual_ingredients
+
 def KNN_fitdata(training_data,cuisine,number):  
     number = int(number)
     values_close = KNeighborsClassifier(n_neighbors=number)
+    values_close(train_data,test_data)
+    y_pred = values_close.predict(test_data)
+    metrics.accuracy_score(test_data, y_pred)
+    values_close.fit(train_data,y_pred)
     return values_close.fit(training_data,cuisine)
 X = cv.fit_transform(train_data['concat_ingredients'].values)
 X
@@ -97,20 +107,46 @@ def KNN_classifier(test_data,nearby,neighbours):
     predicted_single_cuisine = nearby.predict(test_data)
     predicted_class = nearby.classes_
     print ("Cuisine %s" %(predicted_single_cuisine[0]))
-    print ("")
+"""
+#printing the individual cuisine id and the cuisine values predicted as a dataframe
+train.groupby('cuisine').num_ingredients.describe()
+X = train.ingredients_str
+y = train.cuisine
+vect = CountVectorizer()
+X_dtm = vect.fit_transform(X)
+vect = CountVectorizer(token_pattern=r"'([a-z ]+)'")
+knn = KNeighborsClassifier(n_neighbors=100)
+knn.fit(train_data,test_data)
+y_pred=knn.predict(test_data)
+knn.fit(X,y)
+X_new = new[feature_cols]
+new_pred_class_knn = knn.predict(X_new)
+new_pred_class_knn
+#predicted the cuisine
+"""
     for i in range(len(cuisine_predicted)):
         if not(cuisine_predicted[i] == 0.0):
             print ("Cuisine: %s(%f)" %(predicted_class[i],cuisine_predicted[i]*100))
     print ("Closest %d recipes : " % neighbours)
     match_perc,match_id = nearby.kneighbors(test_data)
     for i in range(len(match_id[0])):
-        print ((meal[match_id[0][i]]))
+        print ((meal[match_id[0][i]])) 
+
+
 def classified_data(whole_data):
     training_data = whole_data[:len(whole_data)-1]
     test_data = whole_data[len(whole_data)-1]
     #train_data = pd.read_json('C:/Users/jwala/OneDrive/Desktop/cuisine_train.json')
     #test_data = pd.read_json('C:/Users/jwala/OneDrive/Desktop/cuisine_test.json')
     return (training_data,test_data)
+        if not(cuisine_predicted[i] == 0.0):
+            print ("Cuisine: %s(%f)" %(predicted_class[i],cuisine_predicted[i]*100))
+    print ("Closest %d recipes : " % neighbours)
+    match_perc,match_id = nearby.kneighbors(test_data)
+    for i in range(len(match_id[0])):
+        print ((meal[match_id[0][i]]))
+    X_train_transformed = training_data.transform(training_data.values) #applying vectorizer for x-values of train set
+X_test_transformed = training_data.transform(test_data.values)   #applying vectorizer for x-values of test set
 def sub_function():
     preferred_ingredients = input("Enter the ingredients that you would want to match your cuisine with ")
     whole_data = vectorizer_function(individual_ingredients,preferred_ingredients)
